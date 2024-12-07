46d05f1b5803f0dc644973f639518d6c9b528a47
ander <andersonbarreto2001@gmail.com>
2024-12-07 14:20:39 +0100

    wip network infra

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,3 +1,40 @@
 resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "${var.environment}-public-route-table"
+  }
+}
+
+resource "aws_subnet" "public-subnet" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = element(var.availability_zones, 0)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}-public-subnet-${count.index + 1}"
+  }
 }
