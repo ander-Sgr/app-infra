4c25a0a0bde7caf22823af50ad6ccbd537667326
ander <andersonbarreto2001@gmail.com>
2024-12-07 21:39:05 +0100

    fix: enable public ip on public subnet

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -40,7 +40,7 @@ resource "aws_subnet" "public-subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = element(var.availability_zones, 0)
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.environment}-public-subnet-${count.index + 1}"
@@ -96,4 +96,3 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private.id
 }
-
