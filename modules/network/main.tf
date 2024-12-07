1e6869eb39d2c017964ae33c8765f68f6aabd6b6
ander <andersonbarreto2001@gmail.com>
2024-12-07 14:42:24 +0100

    wip: private network and routing tables

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -14,6 +14,14 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
+# public netowrk
+resource "aws_eip" "nat" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "${var.environment}-nat-eip"
+  }
+}
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
@@ -38,3 +46,54 @@ resource "aws_subnet" "public-subnet" {
     Name = "${var.environment}-public-subnet-${count.index + 1}"
   }
 }
+
+# private network
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = element(var.availability_zones, 0)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.environment}-private-subnet-${count.index + 1}"
+  }
+}
+
+# nat getway for the private network
+resource "aws_nat_gateway" "nat_private_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-subnet[0].id
+  depends_on    = [aws_eip.nat]
+
+  tags = {
+    Name = "${var.environment}-nat-gateway"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_private_gateway.id
+  }
+  tags = {
+    Name = "${var.environment}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public-subnet)
+
+  subnet_id      = aws_subnet.public-subnet[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private_subnet)
+
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
+}
+
